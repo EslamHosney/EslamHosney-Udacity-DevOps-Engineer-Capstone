b97b66cd5215666581af15eea3a83412a22d9a85
EslamHosney <eslamsamymeg@gmail.com>
2021-12-04 16:55:10 +0000

    Add build-image

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=eslamhosney/udacity_devops_engineer_capstone .
+docker build --tag=eslamhosney/udacity_devops_engineer_capstone:latest .
 
 # Step 2: 
 # List docker images
